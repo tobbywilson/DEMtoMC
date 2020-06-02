068488a86129895eef436b1c811cdbfc72509481
Toby <2275540+tobbywilson@users.noreply.github.com>
2020-06-02 18:04:20 +1000

    Create DEMtoMC.py

---
+++ DEMtoMC.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 17:55:53 2020
+
+@author: Toby
+"""
+
+
+import numpy
+import os
+import pandas
+#from osgeo import gdal
+
+import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
